09747eb69bd604f17f5b94f4503c108718fdc08d
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-06-06 00:51:30 +0200

    Clean up code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 const deck = document.querySelector(".deck");
-// const iconsUnique = ["fa-anchor", "fa-bicycle", "fa-bolt", "fa-bomb", "fa-cube", "fa-diamond", "fa-leaf", "fa-paper-plane-o"];
 const iconsUnique = ["fa-gem", "fa-crow", "fa-fighter-jet", "fa-binoculars", "fa-shoe-prints", "fa-skull", "fa-hand-paper", "fa-wrench"];
-
 const icons = [...iconsUnique, ...iconsUnique];
 const star1 = $(".stars li:nth-child(1) i");
 const star2 = $(".stars li:nth-child(2) i");
@@ -20,15 +18,12 @@ let matchedPairs = 0;
 let time;
 let seconds = 0;
 let minutes = 0;
-const modalButton = document.querySelector(".modalButton"); //TEMP
-
 
 // Create a new list of cards based on array and append it to DOM with event listeners
 function createNewDeck() {
     resetDeck();
     shuffle(icons);
     timerListenerOn();
-    console.log(icons);
     for (icon of icons) {
         const newCard = document.createElement("li");
         const newContent = document.createElement("i");
@@ -40,11 +35,9 @@ function createNewDeck() {
     listenToDeckClicks();
 }
 
-
 function resetDeck() {
     $(deck).empty();
 }
-
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -59,34 +52,27 @@ function shuffle(array) {
     return array;
 }
 
-
 function listenToDeckClicks() {
     deck.addEventListener("click", respondToTheClick);
 }
 
-
 function respondToTheClick(clicked) {
     if (clicked.target.nodeName === "LI") {
-        console.log("LI was clicked -> event:");
-        console.log(clicked);
         openCard(clicked);
         countCards(clicked);      
     }
 }
-
 
 function openCard(clicked) {
     clicked.target.className = "card open show freeze";
     countMove();
 }
 
-
 function countMove() {
     moves++;
     checkStars();
     displayMoves();
 }
-
 
 function checkStars() {
     switch(moves) {
@@ -107,47 +93,37 @@ function checkStars() {
     }
 }
 
-
 function emptyStar(star) {
     star.removeClass("fas fa-star");
     star.addClass("far fa-star");
 }
 
-
 function displayMoves() {
-    if (moves === 1) {
-        firstMoveSyntax();
-    } else {
+    if (moves > 1) {
         pluralMovesSyntax();
+    } else {
+        firstMoveSyntax();
     }
 }
-
 
 function firstMoveSyntax() {
     movesContainer.innerHTML = `${moves} Move`;
 }
 
-
 function pluralMovesSyntax() {
     movesContainer.innerHTML = `${moves} Moves`;
 }
 
-
 function countCards(clicked) {
     openCardsList.push(clicked);
-            console.log("CLICKS COUNT: " + openCardsList.length + "\nOPENED CARDS: ");
-            console.log(openCardsList);
     isPair();    
 }
 
-
 function isPair() {
     if (openCardsList.length === 2) {
-            console.log("You just clicked twice. I'm checking for a match: ");
         checkMatch();
     }
 }
-
 
 function checkMatch() {
     let icon1class = openCardsList[0].target.firstChild.className;
@@ -160,35 +136,27 @@ function checkMatch() {
     }
 }
 
-
 function freezeDeck() {
     deck.classList.toggle("freeze");
 }
 
-
 function countMatch() {
     matchedPairs++;
-    console.log("MATCHED PAIRS: " + matchedPairs);
     if (matchedPairs < 8) {
         match();
     } else {
         toggleMatchStyle();
         stopTimer();
-        //youWon();
         openModal();
-        console.log("Y O U   W O N !");
     }
 }
-
 
 function match() {
     toggleMatchStyle();
     emptyOpenCardsList();
 }
 
-
 function toggleMatchStyle() {
-    console.log("It's a match! I'm toggling the match style");
     openCardsList[0].target.className = "card match freeze";
     openCardsList[1].target.className = "card match freeze";
     freezeDeck();
@@ -196,39 +164,30 @@ function toggleMatchStyle() {
 
 
 function shakeCards() {
-    setTimeout( () => {
-        openCardsList[0].target.classList.toggle("shake");
-        openCardsList[1].target.classList.toggle("shake");
-        openCardsList[0].target.classList.toggle("noMatch");
-        openCardsList[1].target.classList.toggle("noMatch");
-    }, 800);
-
+    setTimeout(shakeToggle, 800);
     shakeOff();
     waitToHide();
 }
 
+function shakeToggle() {
+    openCardsList[0].target.classList.toggle("shake");
+    openCardsList[1].target.classList.toggle("shake");
+    openCardsList[0].target.classList.toggle("noMatch");
+    openCardsList[1].target.classList.toggle("noMatch");
+}
 
 function shakeOff() {
-    setTimeout( () => {
-        openCardsList[0].target.classList.toggle("shake");
-        openCardsList[1].target.classList.toggle("shake");
-        openCardsList[0].target.classList.toggle("noMatch");
-        openCardsList[1].target.classList.toggle("noMatch");
-    }, 200);
+    setTimeout(shakeToggle, 200);
 }
-
 
 function waitToHide() {
-    console.log("No match. I'm hiding the cards in 1s...");
     setTimeout(hide, 900);
 }
-
 
 function hide() {
     resetCurrentCards();
     emptyOpenCardsList();
 }
-
 
 function resetCurrentCards() {
     openCardsList[0].target.className = "card";
@@ -236,11 +195,9 @@ function resetCurrentCards() {
     freezeDeck();
 }
 
-
 function emptyOpenCardsList() {
     openCardsList = [];
 }
-
 
 function resetStars() {
     star1[0].className = "fas fa-star";
@@ -250,12 +207,10 @@ function resetStars() {
     star5[0].className = "fas fa-star";
 }
 
-
 function resetMoves() {
     moves = 0;
     movesContainer.innerHTML = "0 Moves";
 }
-
 
 function resetTimer() {
     timerContainer.textContent = "00:00";
@@ -263,31 +218,32 @@ function resetTimer() {
     minutes = 0;
 }
 
-
 function resetCards() {    
-    let cards = document.querySelectorAll(".card");
+    addFlip();
+    setTimeout(removeFlip, 710);
+}
 
+function addFlip() {
+    let cards = document.querySelectorAll(".card");
     for (card of cards) {
         card.classList.add("flipAllCards");
     }
-
-    setTimeout( () => {
-        for (card of cards) {
-            card.classList.remove("flipAllCards");
-        }
-    }, 710);
 }
 
+function removeFlip() {
+    let cards = document.querySelectorAll(".card");
+    for (card of cards) {
+        card.classList.remove("flipAllCards");
+    }
+}
 
 function resetMatchedPairs() {
     matchedPairs = 0;
 }
 
-
 function stopTimer() {
     clearTimeout(time);
 }
-
 
 function countSeconds() {
     seconds++;
@@ -295,83 +251,70 @@ function countSeconds() {
         seconds = 0;
         minutes++;
     }   
-    timerContainer.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + 
-                                 (seconds > 9 ? seconds : "0" + seconds);
+    timerContainer.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
     timer();
 }
-
 
 function timer() {
     timerListenerOff();
     time = setTimeout(countSeconds, 1000);
 }
 
-
 function timerListenerOn() {
     deck.addEventListener("click", timer);
 }
-
 
 function timerListenerOff() {
     deck.removeEventListener("click", timer);
 }
 
-
 function rotateRestartIcon() {
-    restartButton.className = "fas fa-redo-alt rotate-scale";
-    
+    setRotateClass();
     setTimeout(stopRotate, 410);
-
     restart();
 }
 
+function setRotateClass() {
+    restartButton.className = "fas fa-redo-alt rotate-scale";
+}
 
 function stopRotate() {
     restartButton.classList.remove("rotate-scale");
 }
 
-
 function openModal() {
     attachResults();
-
     setTimeout(displayModal, 600);
 }
-
 
 function displayModal() {
     modal.style.display = "block";
 }
 
-  
 function attachResults() {
-      let stars = document.querySelector(".stars").innerHTML;
-      let starsModal = document.querySelector(".starsModal");
-      let timeHTML = timerContainer.innerHTML;
-      let results = `You moved ${moves} times and your time was ${timeHTML}`;
-      let resultsModal = document.querySelector(".results");
-      
-      starsModal.innerHTML = stars;
-      resultsModal.innerHTML = results;
+    let stars = document.querySelector(".stars").innerHTML;
+    let starsModal = document.querySelector(".starsModal");
+    let timeHTML = timerContainer.innerHTML;
+    let results = `You moved ${moves} times and your time was ${timeHTML}`;
+    let resultsModal = document.querySelector(".results");
+    starsModal.innerHTML = stars;
+    resultsModal.innerHTML = results;
 }
-  
-  
+
 function restartFromModal() {
     closeModal();
     restart();
 }
 
-
-function closeModal(){
+function closeModal() {
     modal.style.display = "none";
 }
-  
 
 function outside(click) {
-    if(click.target === modal) {
-      modal.style.display = "none";
+    if (click.target === modal) {
+        modal.style.display = "none";
     }
 }
-
 
 function restart() {
     createNewDeck();
@@ -385,17 +328,10 @@ function restart() {
     timerListenerOn();
 }
 
-
 restartButton.addEventListener("click", rotateRestartIcon);
-
-modalButton.addEventListener("click", openModal); //TEMP
-
 closeButton.addEventListener("click", closeModal);
-
 playAgainButton.addEventListener("click", restartFromModal);
-
 window.addEventListener("click", outside);
-
 
 // Init function
 createNewDeck();
